b8bed259534aa172b91c75199ec5335e86f48037
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-04 13:56:14 +0300

    create counter cubit

--- lib/cubits/cubit/counter_cubit.dart
+++ lib/cubits/cubit/counter_cubit.dart
@@ -5,4 +5,16 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterInitial());
+  int teamACounter = 0;
+  int teamBCounter = 0;
+
+  void incrementTeam({required String team, required int buttonNumber}) {
+    if (team == 'A') {
+      teamACounter += buttonNumber;
+      emit(IncreaseCouterA());
+    } else {
+      teamBCounter += buttonNumber;
+      emit(IncreaseCouterB());
+    }
+  }
 }
